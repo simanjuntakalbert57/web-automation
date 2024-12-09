2669939f499156ede54e4d4edad7f8d96dd89a29
albert.simanjuntak <albert.simanjuntak@bytedance.com>
2024-12-09 19:31:17 +0700

    remove unused lib

--- src/test/java/Locator2.java
+++ src/test/java/Locator2.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-import org.jspecify.annotations.Nullable;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
--- src/test/java/Locator3.java
+++ src/test/java/Locator3.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.asserts.Assertion;
 
 public class Locator3 {
     public static void main(String[] args) throws InterruptedException {
