594ab4058487d376b37b488ac2d3a058fcc531c7
albert.simanjuntak <albert.simanjuntak@bytedance.com>
2024-12-17 21:07:16 +0700

    add pom

---
+++ src/main/java/com/coursewebautomation/abstractcomponents/AbstractComponent.java
@@ -0,0 +1,30 @@
+package com.coursewebautomation.abstractcomponents;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AbstractComponent {
+
+    WebDriver driver;
+    public AbstractComponent(WebDriver driver){
+        this.driver = driver;
+    }
+
+    @FindBy(css = "[routerlink*='cart']")
+    WebElement cartButton;
+
+    public void waitForVisibilityElement(By findBy){
+         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+         wait.until(ExpectedConditions.visibilityOfElementLocated(findBy));
+    }
+
+    public void goToCart(){
+        cartButton.click();
+    }
+}
--- src/test/java/automation/StandAloneTestPO.java
+++ src/test/java/automation/StandAloneTestPO.java
@@ -1,109 +1,51 @@
 package automation;
 
-import java.time.Duration;
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import com.coursewebautomation.pageobjects.CartPage;
+import com.coursewebautomation.pageobjects.CheckoutPage;
+import com.coursewebautomation.pageobjects.ConfirmationPage;
 import com.coursewebautomation.pageobjects.LandingPage;
 import com.coursewebautomation.pageobjects.ProductListPage;
 
 
 public class StandAloneTestPO {
     public static void main(String[] args) throws InterruptedException {
-         // Setup seleniumchromedriver
+        // data
+        String productName = "ZARA COAT 3";     
+        String coutryString = "Indonesia";
+
+        // Setup seleniumchromedriver
         System.setProperty("webdriver.chrome.driver","/Users/bytedance/CourseQAAutomation/Web Automation/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-
         driver.get("https://rahulshettyacademy.com/client");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        String productName = "ZARA COAT 3"; 
-
-        
-        //Scenario login
-        // driver.findElement(By.id("userEmail")).sendKeys("simanjuntakalbert57@gmail.com");
-        // driver.findElement(By.id("userPassword")).sendKeys("XBf@rWNvByn!#K8");
-
-        // driver.findElement(By.id("login")).click();
-
-        // String userEmail = "simanjuntakalbert57"+randInt+"@gmail.com";
-
-        //token -> never expired
-        //login utk token 
-
-        /*
-         * 1. Preconditin di automation , add scenario untuk menambahkan/ increase barang -> seller side -> sebuah API utk increate stock (2000)
-         * 2. then run automation for checkout -> buyer side -> e2e test (2)
-         */
-
+        // scenario login
         LandingPage landingPage = new LandingPage(driver);
         landingPage.loginApplication("simanjuntakalbert57@gmail.com", "XBf@rWNvByn!#K8");
 
+        // scenario add product
         ProductListPage productListPage = new ProductListPage(driver);
-        
         productListPage.addProduct(productName);
+        landingPage.goToCart();
 
-        Thread.sleep(2000);
-
-        driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
-
-        
         // scenario cartpage
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".cartSection h3")));
-
-        List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
-
-        Boolean match = cartProducts.stream().anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName));
-
+        CartPage cartPage = new CartPage(driver);
+        Boolean match = cartPage.verifyCheckoutProduct(productName);
         Assert.assertTrue(match);
-
-        driver.findElement(By.cssSelector(".totalRow button")).click();
+        cartPage.goToCheckoutPage();
 
         // scenario checkout
-        Actions action = new Actions(driver);
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        checkoutPage.selectCountry(coutryString);
+        checkoutPage.submitOrder();
 
-        action.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")),"ind").build().perform();
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
-
-
-        /*
-         * Melakukan looping untuk mencari negara yang diinginkan
-         * 
-         * String country = Indonesia
-         * if (element.gettext() == "indonesia")
-         * element.click()
-         */
-
-        String couString = "Indonesia";
-
-        List<WebElement> countries = driver.findElements(By.xpath("//span[@class='ng-star-inserted']"));
-
-        System.out.println(countries.size());
-
-        WebElement country = countries.stream().filter(cont -> cont.getText().equalsIgnoreCase(couString)).findFirst().orElse(null);
-
-        country.click();
-
-        driver.findElement(By.cssSelector(".action__submit")).click();
-
-
-        // Scenario confirmation page
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".hero-primary")));
-
-        String confirmationPage = driver.findElement(By.cssSelector(".hero-primary")).getText();
-
-        Assert.assertTrue(confirmationPage.equals("THANKYOU FOR THE ORDER."));
-
+        // scenario confirmation page
+        ConfirmationPage confirmationPage = new ConfirmationPage(driver);
+        Assert.assertTrue(confirmationPage.getConfirmationMessage().equals("THANKYOU FOR THE ORDER."));
 
         driver.quit();
 
